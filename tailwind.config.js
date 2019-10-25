4359ec78bce7cb6261495058b6922c014606d4ae
Daniel McFarland <daniel.mcfarland@me.com>
2019-10-25 07:41:39 +0100

    Homepage Layout

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+        colors: {
+          // indigo: '#5c6ac4',
+          blue: {
+              '400': '#4770ac',
+              '500': '#5684c0'
+          }
+          // red: '#de3618',
+        }
+    }
+
+  },
+  variants: {},
+  plugins: []
+}
